4b86b7f4f0c54434e1fd46c9736e2fbc32dfc440
iharmanpannu <iharmanpannu@gmail.com>
2017-12-28 14:40:58 -0800

    added redirect uri

--- src/util/Spotify.js
+++ src/util/Spotify.js
@@ -1,4 +1,5 @@
 const clientId = "a437b2ac07cb4dbb9a59b4b552b0a3de";
+const redirectURI = "http://localhost:3000/";
 
 let userAccessToken;
 
@@ -7,8 +8,13 @@ const Spotify ={
   getAccessToken() {
     if (userAccessToken) {
       return userAccessToken;
+    } else if (window.location.href.match(/access_token=([^&]*)/) != null) {
+      userAccessToken = window.location.href.match(/access_token=([^&]*)/)[0].split("=")[1];
+      let expiresIn = window.location.href .match(/expires_in=([^&]*)/)[0].split("=")[1];
+      window.setTimeout(() => userAccessToken = '', expiresIn * 1000);
+      window.history.pushState('Access Token', null, '/');
     } else {
-
+      const authUri = ``;
     }
   }
 }
