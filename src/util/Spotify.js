e46ea6ff214188309ac2679391b4fe2d274fd68c
iharmanpannu <iharmanpannu@gmail.com>
2017-12-28 14:25:14 -0800

    added variable to spotify method

--- src/util/Spotify.js
+++ src/util/Spotify.js
@@ -1,7 +1,15 @@
-const userAccessToken = '';
+const clientId = "a437b2ac07cb4dbb9a59b4b552b0a3de";
+
+let userAccessToken;
 
 const Spotify ={
 
-}
+  getAccessToken() {
+    if (userAccessToken) {
+      return userAccessToken;
+    } else {
 
+    }
+  }
+}
 export default Spotify
