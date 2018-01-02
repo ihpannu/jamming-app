53c2c48d282cdb573938a1a2ae64d5e5b7be12a6
iharmanpannu <iharmanpannu@gmail.com>
2018-01-02 10:52:15 -0800

    added redirect url

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -27,7 +27,6 @@ class App extends Component {
   //If track is not found in playlist, adds the track
   addTrack(track) {
     let tracks = [...this.state.playlistTracks, track];
-    // tracks.push(track);
 
     this.setState({ playlistTracks: tracks });
   }
@@ -85,100 +84,3 @@ class App extends Component {
 }
 
 export default App;
-
-
-
-
-
-
-
-// import React, {Component} from 'react';
-// import './App.css';
-
-// import SearchBar from '../SearchBar/SearchBar';
-// import Playlist from '../Playlist/Playlist';
-// import SearchResults from '../SearchResults/SearchResults';
-// import Spotify from '../../util/Spotify';
-
-// class App extends Component {
-//   constructor(props) {
-//     super(props);
-//     this.state = {
-//       searchResults: ["name", "artist", "album"],
-//       playlistName: "Punjabi Mix",
-//       playlistTracks: ["name", "artist", "album"]
-//     };
-//     this.addTrack = this.addTrack.bind(this);
-//     this.removeTrack = this.removeTrack.bind(this);
-//     this.savePlaylist = this.savePlaylist.bind(this);
-//     this.search = this.search.bind(this);
-//   }
-
-//   addTrack(track) {
-//     let tracks = this.state.playlistTracks;
-
-//     if (!tracks.some(playlistTrack => playlistTrack.id === track.id)) {
-//       tracks.push(track);
-//       this.setState({ playlistTracks: tracks });
-//     } else {
-//       console.log("Song Already Added to Playlist");
-//     }
-//   }
-
-//   removeTrack(track) {
-//     let newPlaylistTracks = this.state.playlistTracks.filter(
-//       playlistTrack => playlistTrack !== track.id
-//     );
-//     this.setState({
-//       playlistTracks: newPlaylistTracks
-//     });
-//   }
-
-//   updatePlaylistname(name) {
-//     this.setState({ playlistName: name });
-//   }
-
-//   savePlaylist() {
-//     let tracks = this.props.playlistTracks;
-//     let trackUris = tracks.map(track => track.uri);
-//   }
-
-
-//   search(term) {
-//     if (term !== "") {
-//       Spotify.search(term).then(results => {
-//         console.log(results);
-//       });
-//     } else {
-//       this.setState({ searchResults: [] });
-//     }
-//   }
-
-//   render() {
-//     return (
-//       <div>
-//         <h1>
-//           Ja<span className="highlight">mmm</span>ing
-//         </h1>
-//         <div className="App">¯
-//           <SearchBar onSearch={this.search} />
-
-//           <div className="App-playlist">
-//             <SearchResults
-//               searchResults={this.state.searchResults}
-//               onAdd={this.addTrack}
-//             />
-//             <Playlist
-//               playlistName={this.state.playlistName}
-//               playlistTracks={this.state.playlistTracks}
-//               onRemove={this.removeTrack}
-//               onSave={this.savePlaylist}
-//             />
-//           </div>
-//         </div>
-//       </div>
-//     );
-//   }
-// }
-
-// export default App;
