99286d5a2d82e62d9ab595eb7e9ffb1097c11c4c
iharmanpannu <iharmanpannu@gmail.com>
2018-01-05 13:31:30 -0800

    added onkeypress function

--- src/Components/SearchBar/SearchBar.js
+++ src/Components/SearchBar/SearchBar.js
@@ -6,9 +6,10 @@ import "./SearchBar.css";
 class SearchBar extends Component {
   constructor(props) {
     super(props);
-    this.state = { term: '' };
+    this.state = { term: "" };
     this.search = this.search.bind(this);
     this.handleTermChange = this.handleTermChange.bind(this);
+    this.onKeyPressHandler = this.onKeyPressHandler.bind(this);
   }
 
   search() {
@@ -16,16 +17,24 @@ class SearchBar extends Component {
   }
 
   handleTermChange(event) {
-    this.setState({term: event.target.value});
+    this.setState({ term: event.target.value });
+  }
+
+  onKeyPressHandler(event) {
+    if (event.keyCode === 13) {
+      this.props.onSearch(this.state.term);
+    }
   }
 
   render() {
     return (
       <div className="SearchBar">
-        <input placeholder="Enter a Song, Album, or Artist" 
-        onChange={this.handleTermChange}
+        <input
+          placeholder="Enter a Song, Album, or Artist"
+          onChange={this.handleTermChange}
+          onKeyDown={this.onKeyPressHandler}
         />
-        <a onClick={this.search} >Search</a>
+        <a onClick={this.search}>Search</a>
       </div>
     );
   }
